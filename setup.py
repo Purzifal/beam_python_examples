8cf5d5b10505a9bbd274a72dcb32fa4a4f2c5c22
CAPGames <gaming@capgames.co.uk>
2017-04-22 02:06:47 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='beam_python_examples',
+      version='1.0',
+      description='Examples on how to do stuff with Beam',
+      author='CAPGames',
+      author_email='spectrumbot@capgames.co.uk',
+      url='https://github.com/capgames/beam_python_examples',
+      packages=['websocket-client', 'requests_oauthlib'],
+)
