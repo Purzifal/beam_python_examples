b7b2236949ddba4c2e2d527aca333e282f0d3228
CAPGames <gaming@capgames.co.uk>
2017-05-08 14:04:27 +0100

    Added interactive 2 example
    Added interactive 2 example code and renamed beam_interactive_example.py
    to beam_interactive_1_example.py and updated config.py with interactive
    2 details

---
+++ beam_interactive_2_example.py
@@ -0,0 +1,92 @@
+'''
+Connects to the Beam Interactive 2.0 and outputs
+recieved data to the console
+'''
+
+import asyncio
+from json import dumps, loads
+from random import randint
+
+import requests
+import websocket
+
+import config
+
+# enables a detailed trace of what is happening when connecting
+# though websockets, Change to False to switch it off
+websocket.enableTrace(False)
+# Creates the websocket Object
+WS = websocket.WebSocket()
+CONFIG = config
+
+def _buildurl(path):
+    return CONFIG.BEAM_URI + path
+
+def get_endpoints():
+    '''Gets the Interactive 2.0 server addresses'''
+    data = dict(client_id=config.CLIENT_ID)
+    # Creates the header for the request
+    header = {'Media-Type': 'application/json',
+              'Authorization': 'Bearer ' + CONFIG.ACCESS_TOKEN}
+    # Get the request and return the responce
+    url = _buildurl(CONFIG.INTERACTIVEHOSTS_URI)
+    return requests.get(url=url, data=data, headers=header).json()
+
+def _random_endpoint(endpoint):
+    rnd = randint(0, len(endpoint) - 1)
+    return endpoint[rnd]['address']
+
+@asyncio.coroutine
+def message(msg):
+    '''Sends a chat message.'''
+    # send the packet to the data
+    WS.send(dumps(msg))
+
+@asyncio.coroutine
+def connect_interactive():
+    '''Connects to a randomly selected interactive 2.0 server and authenticates'''
+    # gets a random server endpoint
+    endpoint = _random_endpoint(get_endpoints())
+
+    # print to console that connecting to a server
+    print('Connecting to {} Interactive 2.0 server...'.format(endpoint))
+
+    # Generates header for the connection to the endpoint (REQUIRED)
+    header = ['Authorization: Bearer {}'.format(CONFIG.ACCESS_TOKEN),
+              'X-Interactive-Version: {}'.format(CONFIG.GAME_ID),
+              'X-Protocol-Version: 2.0']
+    # connect to server
+    WS.connect(endpoint, header=header)
+
+
+@asyncio.coroutine
+def recieve_interactive():
+    '''listens for interactive events and prints to the console'''
+
+    # create a Ready payload
+    payload = {"type": "method",
+               "id": 0,
+               "method": "ready",
+               "params": {"isReady": True},
+               "discard": True}
+    print('Sending Ready message to the connected server')
+    # sends the Ready payload to the connected server
+    yield from message(payload)
+
+    # loop while connected
+    while WS.connected:
+        # This can be change to however you wich to handle
+        # the interactive events
+        print(loads(WS.recv()))
+    # close connection when disconnecting
+    WS.close()
+
+
+if __name__ == '__main__':
+    LOOP = asyncio.get_event_loop()
+
+    try:
+        LOOP.run_until_complete(connect_interactive())
+        LOOP.run_until_complete(recieve_interactive())
+    finally:
+        LOOP.close()
--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 '''config file'''
 
-# Beam Base URL
+# DO NOT CHANGE ANY OF THESE SETTINGS
+# Beam Base API URL
 BEAM_URI = 'https://beam.pro/api/v1/'
 
 # API Endpoints
@@ -9,20 +10,32 @@ AUTHTOKEN_URI = 'oauth/token'
 USERSCURRENT_URI = 'users/current'
 CHATSCID_URI = 'chats/{cid}'
 INTERACIVE_URI = "interactive/{channel}/robot"
+INTERACTIVEHOSTS_URI = 'interactive/hosts'
 
-# Redirect URL as stated in the OAUth Client under Website
 
+# YOU CAN CHANGE THESE SETTINGS
+# Redirect URL as stated in the OAUth Client under Website from
+# https://beam.pro/lab and click AOUTH CLIENTS
 REDIRECT_URI = 'https://auth.example.com'
 
-# This is up to you to obtain
+# This is up to you to obtain, you may want to look at the 
+# beam_oauth_example.py or you can find details about this  at
+# https://dev.beam.pro/reference/oauth/index.html
 ACCESS_TOKEN = 'REPLACE_WITH_YOUR_OAUTH_ACCESS_TOKEN'
 
-# Provided when OAuth app is created
+# Provided when OAuth app is created, you can get this from
+# https://beam.pro/lab and click AOUTH CLIENTS
 CLIENT_ID = 'REPLACE_WITH_YOUR_CLIENT_ID'
 
-# Permission for the chatbot
+# Permission for the chatbot a list of scopes can be found at
+# https://dev.beam.pro/reference/oauth/index.html
 SCOPE = ('channel:details:self channel:update:self chat:chat chat:connect '
          'chat:giveaway_start interactive:robot:self user:details:self')
+
+# This is for interactive 2.0 and you can get this by going to
+# https://beam.pro/i/studio go to the editor of your project and 
+# going to the code tab at the top of the page
+GAME_ID = '12345'
 
 # Packet ID Number
 PACKETID = 0
